38786324c38aea04922e8d6e2beeb5596888d786
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-05-24 15:46:16 -0400

    eliminacion

--- pokeapi/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Header = () => {
-  return (
-    <div>Hola soy el Header</div>
-  )
-}
--- pokeapi/src/pages/PokePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const PokePage = () => {
-  return (
-    <div>PokePage</div>
-  )
-}
